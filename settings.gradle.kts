cba6bcfd6ca863c92a9e8f20808eaa7358a60f52
favour@swiftcoretech.com <favour@swiftcoretech.com>
2026-02-21 19:33:09 +0000

    Fix Gradle plugin repositories and root plugin declarations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "crm247-tracker-android"
 include(":tracker")
